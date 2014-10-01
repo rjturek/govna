7e3560f9ae8583ac3869cd624eeb566cdeb16057
Richard Turek <rjt1357@gmail.com>
2014-10-01 16:22:26 -0400

    jslint stuff

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -1,21 +1,23 @@
+/*global angular*/    // Stop jsLint from complaining about globally defined variables.
+
 //var app = angular.module('govna', ['ui.bootstrap']);
 var app = angular.module('govna', []);
 
-app.controller('MainCtrl', function($scope, $http) {
-    var part1 = 'Wo'
-    var part2
-    if (part1 == 'xyz') {
-        part2 = 'ert'
-    }
-    else {
-        part2 = 'rld'
+app.controller('MainCtrl', function ($scope, $http) {
+    'use strict';
+    var part1 = 'Wo';
+    var part2 = 'sadf';
+    if (part1 === 'xyz') {
+        part2 = 'ert';
+    } else {
+        part2 = 'rld';
     }
     $scope.name = part1 + part2;
 
 //    console.log("somedidlything")
 
     $http.get("http://localhost:8080/api/consumer")
-        .then(function(response){
-        $scope.resp = response.data;
-    })
+        .then(function (response) {
+            $scope.resp = response.data;
+        });
 });
