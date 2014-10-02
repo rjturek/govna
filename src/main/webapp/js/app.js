44dfb2cca4127b0ab5c45c9e45ff1a8b8f99c43c
Richard Turek <rjt1357@gmail.com>
2014-10-02 09:56:41 -0400

    add button and click function

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -1,23 +1,23 @@
 /*global angular*/    // Stop jsLint from complaining about globally defined variables.
 
-//var app = angular.module('govna', ['ui.bootstrap']);
-var app = angular.module('govna', []);
+var app = angular.module('govna', ['ui.bootstrap']);
+//var app = angular.module('govna', []);
 
 app.controller('MainCtrl', function ($scope, $http) {
     'use strict';
     var part1 = 'Wo';
-    var part2 = 'sadf';
-    if (part1 === 'xyz') {
-        part2 = 'ert';
-    } else {
-        part2 = 'rld';
-    }
+    var part2 = 'rld';
     $scope.name = part1 + part2;
 
-//    console.log("somedidlything")
+//    console.log("Calling api/consumer")
+//    $http.get("http://localhost:8080/api/consumer")
+//        .then(function (response) {
+//            $scope.resp = response.data;
+//        });
 
-    $http.get("http://localhost:8080/api/consumer")
-        .then(function (response) {
-            $scope.resp = response.data;
-        });
+    //alert("hey");
+
+    $scope.buttonClicked = function () {
+        alert("you clicked me");
+    };
 });
