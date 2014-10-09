1d33ba86402cc4ddb0c7b3d98095af3ee3ede37e
sunckell <sunckell@gmail.com>
2014-10-09 10:15:25 -0400

    initial commit for consumer group dependencies validation via POST

---
+++ src/main/groovy/com/rjturek/amp/govna/utility/CustomLogFormatter.groovy
@@ -0,0 +1,25 @@
+package com.rjturek.amp.govna.utility
+
+import java.util.logging.LogRecord
+import java.util.logging.Formatter
+
+
+/**
+ * Created by ckell on 10/9/14.
+ */
+public class CustomLogFormatter  extends Formatter  {
+
+    public String format(LogRecord log) {
+        Date date = new Date(log.getMillis())
+        String level = log.getLevel().getName()
+        //String message = "[custom] " + " " + date.toString() + " " + level.toUpperCase() + " "
+        String message = "[custom] " + level.toUpperCase() + " " + log.getSourceClassName() + " "
+        message = message + log.getMessage() + "\n";
+
+        Throwable thrown = log.getThrown();
+        if (thrown != null) {
+            message = message + thrown.toString();
+        }
+        return message;
+    }
+}
