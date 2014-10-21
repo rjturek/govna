e172a901ed9c13d9ac1f313e95f9b0ef928129af
Richard Turek <rjt1357@gmail.com>
2014-10-21 09:07:44 -0400

    CORS needs to specify allowed HTTP methods (not sure why this wasn't needed until now)

--- src/main/groovy/com/rjturek/amp/govna/filter/CorsResponseFilter.groovy
+++ src/main/groovy/com/rjturek/amp/govna/filter/CorsResponseFilter.groovy
@@ -17,7 +17,7 @@ class CorsResponseFilter implements ContainerResponseFilter {
         MultivaluedMap<String, Object> headers = responseContext.getHeaders()
 
         headers.add("Access-Control-Allow-Origin", "*")
-        //headers.add("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT")
+        headers.add("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT")
         //headers.add("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, X-Codingpedia")
     }
 }
