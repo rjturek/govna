95208d46ba393226d445afeadaabe8502c77b3b8
Richard Turek <rjt1357@gmail.com>
2014-10-22 12:50:21 -0400

    ignore extra fields coming in to be deserialized

--- src/main/groovy/com/rjturek/amp/govna/dataobj/Restriction.groovy
+++ src/main/groovy/com/rjturek/amp/govna/dataobj/Restriction.groovy
@@ -1,7 +1,14 @@
 package com.rjturek.amp.govna.dataobj
 
+import com.fasterxml.jackson.annotation.JsonIgnore
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+
+@JsonIgnoreProperties(ignoreUnknown=true)
+
 class Restriction {
+    @JsonIgnore
     static final String TYPE_DEPRECATED = "D"
+    @JsonIgnore
     static final String TYPE_PROHIBITED = "P"
     String type
     String message
